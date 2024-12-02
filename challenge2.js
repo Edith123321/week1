7aa6aa56d46f9ab84f34b1359822d5287a5f8ff3
edith.githinji <edithgithinji2020@gmail.com>
2024-12-02 21:18:36 +0300

    suspend license if the speed limit is crossed

---
+++ challenge2.js
@@ -0,0 +1,20 @@
+//introduce the function
+function checkSpeed(speed){
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    if (speed <= speedLimit) {
+        return "Ok";
+    }
+    //loop the function 
+    let points = 0;
+    for (let currentSpeed = speedLimit + 1; currentSpeed <= speed; currentSpeed += kmPerPoint) 
+        points++;
+    //check if points exceed the suspensuion threshold
+    if (points > 12) {
+        return "License suspended";
+    } else {
+        return `Points: ${points}`;
+    }
+}
+//test the function with a sample speed 
+console.log(checkSpeed(85));
